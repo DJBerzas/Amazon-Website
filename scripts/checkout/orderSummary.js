9706805fff7470fa6f2af24d5f418eb19febe1c2
DJBerzas <djberzas@gmail.com>
2024-06-18 22:21:24 -0400

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,12 +3,9 @@ import{products,getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOptions} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
-const today = dayjs();
 
-
- 
-const deliveryDate = today.add(7,'days');
 
 export function renderOrderSummary(){
 
@@ -128,6 +125,8 @@ export function renderOrderSummary(){
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
+      renderPaymentSummary();
+
       
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
     container.remove();
@@ -141,6 +140,7 @@ export function renderOrderSummary(){
         const {productId,deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
     })
   });
 }
