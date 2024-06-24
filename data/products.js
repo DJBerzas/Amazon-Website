65b71c6cd301493001686d2921879f67f90793bd
DJBerzas <djberzas@gmail.com>
2024-06-24 13:55:18 -0400

    learn promises and convert code to promises

--- data/products.js
+++ data/products.js
@@ -99,7 +99,7 @@ export function loadProducts(fun){
     }
    return new Product(productDetails);
   });
-  console.log('load products');
+  
 
   fun();
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,50 @@ import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 //import '../data/cart-class.js';
 import { loadProducts } from '../data/products.js';
-loadProducts(() =>{
+import { loadCart } from '../data/cart.js';
+
+
+Promise.all([
+  new Promise((resolve) =>{
+    loadProducts(() =>{
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+/*
+new Promise((resolve) =>{
+  loadProducts(() =>{
+    resolve();
+  });
+}).then(() => {
+  return new Promise((resolve) => {
+    
+    loadCart(() => {
+      resolve();
+    });
+  })
+}).then(()=> {
+  renderOrderSummary();
+  renderPaymentSummary();
+
+})
+
+/*
+loadProducts(() =>{
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+
+  })
+});
+*/
